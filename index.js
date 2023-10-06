a108842c39503fc2165615bf1e3eaa33d3447b4f
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-06 23:29:28 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers() {
+  return drivers.slice(0,2);
+}
+function returnLastTwoDrivers() {
+  return drivers.splice(2,3)
+}
+const selectingDrivers = [
+  returnFirstTwoDrivers, 
+  returnLastTwoDrivers
+];
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+function fareDoubler(doubler){
+ return fareDoubler((doubler)*2); 
+ fareDoubler(10)
+}
+fareDoubler(10);
+
+function fareTripler(tripleFare){
+
+}
+function selectDifferentDrivers(arrayOfDrivers, driverSelectorFunction){
+  return driverSelectorFunction(arrayOfDrivers);
+
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
